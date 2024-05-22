1d7734640e5ddf84943f3f70cb86f1f8d02897e9
ChriTorr <christian.torli@itiszuccante.edu.it>
2024-05-22 17:35:00 +0200

    screen

--- core/src/com/mygdx/game/GarbageCollection.java
+++ core/src/com/mygdx/game/GarbageCollection.java
@@ -8,38 +8,23 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.entities.Player;
 import com.mygdx.genericClasses.resources.ResourceEnum;
 import com.mygdx.genericClasses.Utils;
+import com.mygdx.screens.MainScreen;
 
-public class GarbageCollection extends ApplicationAdapter {
-	Stage stage;
-	Texture backGround;
+public class GarbageCollection extends Game {
+
 	
 	@Override
 	public void create () {
-		stage = new Stage(new ScreenViewport());
-		Gdx.input.setInputProcessor(stage);
-		backGround = Utils.getTexture(ResourceEnum.BACKGROUND);
-
-		Player player = new Player(640/2, 480/2);
-		player.setMovementStyle(Player.Styles.REALTIME);
-		stage.addActor(player);
-		stage.setKeyboardFocus(player);
+		setScreen(new MainScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1,1,1,0);
-		ScreenUtils.clear(1,1,1,0);
-
-		stage.getBatch().begin();
-		stage.getBatch().draw(backGround, 0, 0, 640, 480);
-		stage.getBatch().end();
-
-		stage.act(Gdx.graphics.getDeltaTime());
-		stage.draw();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		stage.dispose();
+
 	}
 }
---
+++ core/src/com/mygdx/screens/MainScreen.java
@@ -0,0 +1,54 @@
+package com.mygdx.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.mygdx.entities.Player;
+import com.mygdx.game.GarbageCollection;
+import com.mygdx.genericClasses.Utils;
+import com.mygdx.genericClasses.resources.ResourceEnum;
+
+public class MainScreen extends ScreenAdapter {
+
+    GarbageCollection game;
+    Stage stage;
+    Texture backGround;
+
+    public MainScreen(GarbageCollection game){
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        backGround = Utils.getTexture(ResourceEnum.BACKGROUND);
+
+        Player player = new Player(640/2, 480/2);
+        player.setMovementStyle(Player.Styles.REALTIME);
+        stage.addActor(player);
+        stage.setKeyboardFocus(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,1,1,0);
+        ScreenUtils.clear(1,1,1,0);
+
+        stage.getBatch().begin();
+        stage.getBatch().draw(backGround, 0, 0, 640, 480);
+        stage.getBatch().end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+    }
+}
