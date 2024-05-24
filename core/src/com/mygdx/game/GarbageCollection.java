f67469ba7054d6dd3c40fd83ffcfedf2db4733be
oldtonPear <christian.torli@itiszuccante.edu.it>
2024-05-24 09:29:44 +0200

    .

--- core/src/com/mygdx/game/GarbageCollection.java
+++ core/src/com/mygdx/game/GarbageCollection.java
@@ -1,13 +1,6 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import com.mygdx.entities.Player;
-import com.mygdx.genericClasses.resources.ResourceEnum;
-import com.mygdx.genericClasses.Utils;
 import com.mygdx.screens.MainScreen;
 
 public class GarbageCollection extends Game {
